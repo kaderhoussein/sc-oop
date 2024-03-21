1406216a59ee7bd41697aa604fc0684879fc2483
Benamad Kader HOUSSEIN <90526929+kaderhoussein@users.noreply.github.com>
2024-03-21 22:27:56 +0100

    Update pli.py

--- scoop/pli.py
+++ scoop/pli.py
@@ -1,18 +1,9 @@
 import scanpy
 import plotly
 
-from .plotting._qc import highest_expr_genes
-from .plotting._anndata import violin
-from .plotting._qc import rank_genes_groups_violin ##
-from .plotting.tools.scatterplot import embedding, umap, tsne, pca
+from .plotting.tools.scatterplot import diffmap
 
 __all__ = [
-    "highest_expr_genes",
-    "violin",
-    # "scatter"
-    "rank_genes_groups_violin",
-    "embedding",
-    "umap",
-    "tsne",
-    "pca",
+
+    "diffmap",
 ]
